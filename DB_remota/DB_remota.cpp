281812e2cb8fb261fd088405417c2cefd41da390
alse-a <andres.arias-d@mail.escuelaing.edu.co>
2020-07-16 03:14:09 +0000

    Cambios BD remota

--- DB_remota/DB_remota.cpp
+++ DB_remota/DB_remota.cpp
@@ -3,7 +3,7 @@
  */
 
 #include <iostream>
-#include <mysql/mysql.h>
+#include <mysql.h>
 #include "DB_remota.h"
 #include "DB_local.h"
 #include "Dato.h"
@@ -15,26 +15,34 @@
 
 using namespace std;
 
-int DB_remota::guardar_dato(Dato d, int h)
+bool DB_remota::guardar_dato(Dato d, int h)
 {
-    Dato m;
-    m.getpromedio();
     conectar_DB();
-
-    c = mysql_query( connection, "INSERT INTO (Temperatura, Humedad, Velo_viento, Dir_viento, Latitud, longitud, altura);"
-        "VALUES");
+    c = mysql_query( connection, ;
+    "INSERT INTO TBL_Datos (Hora, Temperatura, Humedad,";
+    " Vel_viento, Dir_viento, Latitud, Longitud, Altura)";
+    " VALUES ( ";
+    h << ", ";
+    d.getTemperatura() << ", ";
+    d.getHumedad() << ", ";
+    d.getVeloviento() << ", ";
+    d.getDirviento() << ", ";
+    d.getLatitud() << ", ";
+    d.getLongitud() << ", ";
+    d.getAltura() << "); ";)
 
     desconectar_DB();
+    return true;
 }
-int DB_remota::conectar_DB()
+bool DB_remota::conectar_DB()
 {
     connection = mysql_conect(&mysql);
     connection = mysql_real_connect(&mysql,HOST,USER,PASSWD,DB,51000,0,0);
 
     if (connection == NULL) {
         cout << mysql_error(&mysql) << endl;
-        return 1;
-    }else{ return 0; }
+        return false;
+    }else{ return true; }
 }
 int DB_remota::desconectar_DB()
 {
